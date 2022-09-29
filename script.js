155cb84e31c2ad880c12005feb2d048c22545f12
Megan Lu <88399770+meganklu@users.noreply.github.com>
2022-09-29 21:33:01 +0000

    mobile nav bar (no animations)

---
+++ script.js
@@ -0,0 +1,9 @@
+// HAMBURGER NAV BAR FOR MOBILE
+function mobileNavFunction() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
