0792ca4f3f74cfac932a251c00209261b0ee27a2
Megan Lu <88399770+meganklu@users.noreply.github.com>
2022-09-30 00:32:40 +0000

    get nav bar back in wide screen after closing with hamburger bar

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function mobileNavFunction() {
     x.style.display = "block";
   }
 }
+
+// GET NAV BAR BACK AFTER CLOSED BY HAMBURGER BAR AND SCREEN BIG
+function navBarBack(screenSize) {
+  if (screenSize.matches) { // If media query matches
+    document.getElementById("navLinks").style.display = "none";
+  } else {
+    document.getElementById("navLinks").style.display = "inline";
+  }
+}
+var screenSize = window.matchMedia("(max-width: 600px)")
+navBarBack(screenSize) // Call listener function at run time
+screenSize.addListener(navBarBack) // Attach listener function on state changes
