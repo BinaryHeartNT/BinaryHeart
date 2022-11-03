29a5ba6d6711c44ca2319b85cb6a7a2a5357410a
Megan Lu <88399770+meganklu@users.noreply.github.com>
2022-11-03 21:21:17 +0000

    center mobile nav links

--- script.js
+++ script.js
@@ -19,13 +19,3 @@ function navBarBack(screenSize) {
 var screenSize = window.matchMedia("(max-width: 600px)")
 navBarBack(screenSize) // Call listener function at run time
 screenSize.addListener(navBarBack) // Attach listener function on state changes
-
-// TRANSLATION VARIABLE FOR MOBILE NAV BAR (from https://www.w3schools.com/css/css3_variables_javascript.asp)
-// Get the root element
-var r = document.querySelector(':root');
-
-// Create a function for setting a variable value
-function setTranslateVariable() {
-  var translationValue = -(((window.innerWidth)-45)/2)
-  r.style.setProperty('--centerMobileNav', translationValue + "px");
-}
